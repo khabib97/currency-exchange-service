dfab4d27651aa0b34a18b02858cfd626e762e5a0
khabib97 <kawser.habib.dev@gmail.com>
2021-03-24 19:12:33 +0600

    bulkhead config done

--- src/main/java/com/old/school/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/old/school/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -25,6 +27,8 @@ public class CircuitBreakerController {
 	
 	@GetMapping("/sample-api-circuit-breaker")
 	@CircuitBreaker(name="default ", fallbackMethod = "hardCodedResponse")
+	@RateLimiter(name="default") // during 10s allow 10000 calls to sample api
+	@Bulkhead(name="default")	//concurrent call allow
 	public String sampleApiCircuiteBreaker() {
 		logger.info("Sample api called received");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
