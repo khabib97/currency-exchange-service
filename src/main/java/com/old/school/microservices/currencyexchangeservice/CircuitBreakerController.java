eeb9aded36c81e87dbbae48accbfd5485758b22e
khabib97 <kawser.habib.dev@gmail.com>
2021-03-24 19:02:05 +0600

    play with circuit breaker

--- src/main/java/com/old/school/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/old/school/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -14,9 +15,17 @@ public class CircuitBreakerController {
 	
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	
-	@GetMapping("sample-api")
+	@GetMapping("/sample-api")
 	@Retry(name="sample-api", fallbackMethod = "hardCodedResponse")
 	public String sampleApi() {
+		logger.info("Sample api called received");
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
+		return forEntity.getBody();
+	}
+	
+	@GetMapping("/sample-api-circuit-breaker")
+	@CircuitBreaker(name="default ", fallbackMethod = "hardCodedResponse")
+	public String sampleApiCircuiteBreaker() {
 		logger.info("Sample api called received");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
 		return forEntity.getBody();
