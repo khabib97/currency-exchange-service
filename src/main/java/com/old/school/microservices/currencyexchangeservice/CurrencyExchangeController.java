0a1b036968ebe4534d62da1874169c8e1cef8dcf
khabib97 <kawser.habib.dev@gmail.com>
2021-03-28 18:15:56 +0600

    Add zipkin support for distributed tracing

--- src/main/java/com/old/school/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/old/school/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,16 +1,20 @@
 package com.old.school.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+
 @RestController
 public class CurrencyExchangeController { 
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	@Autowired
 	private CurrecyExchangeRepository currecyExchangeRepository;
 	
@@ -20,6 +24,7 @@ public class CurrencyExchangeController {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		//CurrencyExchange currencyExchange = new CurrencyExchange(1000L,from,to,BigDecimal.valueOf(50));
+		logger.info("RetrieveExchangeValue called with {} to {}",from,to);
 		CurrencyExchange currencyExchange = currecyExchangeRepository.findByFromAndTo(from, to);
 		if(currencyExchange == null) {
 			throw new RuntimeException("Unable to find data for "+from+ " to "+to );
